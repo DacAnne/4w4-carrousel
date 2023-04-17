7f4730b6b4e96940155e2571144a633db628b8af
Dac Anne Nguyen <nguyenanne066@gmail.com>
2023-04-17 12:57:36 -0400

    s12c1 création dynamique des radio-boutons et modification de l'affichage des boutons radio

--- js/carrousel.js
+++ js/carrousel.js
@@ -4,6 +4,8 @@
     let carrousel = document.querySelector('.carrousel');
     let carrousel__x = document.querySelector('.carrousel__x');
     let carrousel__figure = document.querySelector('.carrousel__figure');
+    let carrousel__form = document.querySelector('.carrousel__form');
+    //console.log(carrousel__form.tagName) conteneur de radio-boutons
 
     let galerie = document.querySelector('.galerie');
     let galerie__img = galerie.querySelectorAll('img');
@@ -15,17 +17,40 @@
     carrousel__x.addEventListener('mousedown',function(){
         carrousel.classList.remove('carrousel--activer');
     });
-
+    /**
+     * Pour chaque image de la galerie l'ajouter dans le carrousel
+     */
+     let position = 0;
     function ajouter_les_images_de_galerie()
-    {
+    {   
         for (const elem of galerie__img){
+          ajouter_une_image_dans_carrousel(elem);
+          ajouter_un_radio_bouton_dans_carrousel();
+           
+        }
+    }
+
+    /**
+     * Création dynamique d'une image pour le carrousel
+     * @param {*} elem une image de la galerie
+     */
+    function ajouter_une_image_dans_carrousel(elem){
             //console.log(elem.getAttribute('src'));
             //console.log(img.src);
             let img = document.createElement('img');
             img.classList.add('carrousel__img');
             img.src = elem.src;
             carrousel__figure.appendChild(img);
-           
-        }
+    }
+
+    function ajouter_un_radio_bouton_dans_carrousel(){
+        let rad = document.createElement('input');
+        rad.setAttribute('type','radio');
+        rad.setAttribute('name','carrousel__rad');
+        rad.classList.add('carrousel__rad');
+        rad.dataset.index = position;
+        position = position + 1;
+        carrousel__form.append(rad);
+
     }
 })()
