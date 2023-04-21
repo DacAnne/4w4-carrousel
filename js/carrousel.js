cc9a47b4a33f642e2f57ab8d6a50531fe6b42221
Dac Anne Nguyen <nguyenanne066@gmail.com>
2023-04-21 09:26:24 -0400

    s12c1 - ajout de la fonction afficher_une_image_dans_carrousel, ajout d'une possibilité d'optimisation de code

--- js/carrousel.js
+++ js/carrousel.js
@@ -53,15 +53,25 @@
         rad.dataset.index = position;
         rad.addEventListener('mousedown', function(){
             index = this.dataset.index;
-            if(ancienIndex != -1){
-                carrousel__figure.children[ancienIndex].style.opacity = "0"
-            }
-            //console.log(this.dataset.index);
-            carrousel__figure.children[index].style.opacity = "1";
-            ancienIndex = index;
+            affiche_image_carrousel;
+           
         });
         position = position + 1; //incrémentation de la position
         carrousel__form.append(rad);
 
+    }
+    /**
+     * Affiche la nouvelle image du carrousel
+     */
+
+    function affiche_image_carrousel(){
+        if(ancienIndex != -1){
+            carrousel__figure.children[ancienIndex].style.opacity = "0"
+            // carrousel__figure.children[ancienIndex].classList,remove('carrousel__img--activer'); // autre façon
+        }
+        //console.log(this.dataset.index);
+        carrousel__figure.children[index].style.opacity = "1";
+        // carrousel__figure.children[index].classList,add('carrousel__img--activer'); // autre façon
+        ancienIndex = index;
     }
 })()
